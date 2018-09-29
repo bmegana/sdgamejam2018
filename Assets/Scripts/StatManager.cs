99d9cd8dbfa62b72d93ae7885d3023b4240d6006
Brian Egana <brianm.egana@gmail.com>
2018-09-29 15:03:40 -0700

    created StatManager, made bars for stats

---
+++ Assets/Scripts/StatManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StatManager : MonoBehaviour
+{
+    public static StatManager instance;
+
+    public float initHealth;
+    private float currentHealth;
+    public Slider healthSlider;
+
+    public float initMoney;
+    private float currentMoney;
+    public Slider moneySlider;
+
+    public float initMorale;
+    private float currentMorale;
+    public Slider moraleSlider;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        currentHealth = initHealth;
+    }
+
+    public void IncreaseHealth(float value)
+    {
+        currentHealth += value;
+        healthSlider.value = currentHealth / initHealth;
+    }
+
+    public void DecreaseHealth(float value)
+    {
+        currentHealth -= value;
+        healthSlider.value = currentHealth / initHealth;
+    }
+
+    public void IncreaseMoney(float value)
+    {
+        currentMoney += value;
+        moneySlider.value = currentMoney / initMoney;
+    }
+
+    public void DecreaseMoney(float value)
+    {
+        currentMoney -= value;
+        moneySlider.value = currentMoney / initMoney;
+    }
+
+    public void IncreaseMorale(float value)
+    {
+        currentMorale += value;
+        moraleSlider.value = currentMorale / initMorale;
+    }
+
+    public void DecreaseMorale(float value)
+    {
+        currentMorale -= value;
+        moraleSlider.value = currentMorale / initMorale;
+    }
+}
